3d9173ead2d3181affa8504b126babb2e5788df9
Roman Masliak <maslyak3@gmail.com>
2024-10-30 11:28:05 +0200

    feat. finish task

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const GuessingGame = require('./src/guessing-game.js');
+class GuessingGame {
+  constructor() {
+    this.min = null;
+    this.max = null;
+    this.currentGuess = null;
+  }
 
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.currentGuess = Math.floor((this.min + this.max) / 2);
+  }
+
+  guess() {
+    this.currentGuess = Math.floor((this.min + this.max) / 2);
+    return this.currentGuess;
+  }
+
+  lower() {
+    this.max = this.currentGuess - 1;
+  }
+
+  greater() {
+    this.min = this.currentGuess + 1;
+  }
+}
 window.game = new GuessingGame();
+
